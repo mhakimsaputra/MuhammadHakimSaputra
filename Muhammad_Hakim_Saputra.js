3467f4ea25334222f2b6b42ac5bea4b01705210b
MACINTOSH <macintosh@MACINTOSHs-MacBook-Pro.local>
2024-09-05 22:35:58 +0700

    menambah hitung luas persegi

--- Muhammad_Hakim_Saputra.js
+++ Muhammad_Hakim_Saputra.js
@@ -3,10 +3,26 @@ const readline = require('readline').createInterface({
     output: process.stdout
   });
   
-  readline.question('Masukkan nilai alas: ', (alas) => {
-    readline.question('Masukkan nilai tinggi: ', (tinggi) => {
-      const luas = 0.5 * alas * tinggi;
-      console.log(`Luas segitiga adalah: ${luas}`);
+  // Fungsi untuk menghitung luas segitiga
+  function hitungLuasSegitiga() {
+    readline.question('Masukkan nilai alas: ', (alas) => {
+      readline.question('Masukkan nilai tinggi: ', (tinggi) => {
+        const luasSegitiga = 0.5 * alas * tinggi;
+        console.log(`Luas segitiga adalah: ${luasSegitiga}`);
+        // Memanggil fungsi untuk menghitung luas persegi setelah selesai
+        hitungLuasPersegi();
+      });
+    });
+  }
+  
+  // Fungsi untuk menghitung luas persegi
+  function hitungLuasPersegi() {
+    readline.question('Masukkan panjang sisi persegi: ', (sisi) => {
+      const luasPersegi = sisi * sisi;
+      console.log(`Luas persegi adalah: ${luasPersegi}`);
       readline.close();
     });
-  });
+  }
+  
+  // Memanggil fungsi untuk menghitung luas segitiga
+  hitungLuasSegitiga();
